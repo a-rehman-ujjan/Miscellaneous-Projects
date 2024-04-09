31d28ac0da2c16a3ca0846ac809efc952f3997c1
Abdul Rehman Ujjan <117349681+a-rehman-ujjan@users.noreply.github.com>
2024-04-09 22:17:34 +0500

    Update script.js

--- Guess The Number (HTML, CSS, & JS)/script.js
+++ Guess The Number (HTML, CSS, & JS)/script.js
@@ -8,9 +8,9 @@ let i = 0;
 const f1 = () => {
   input_number = document.getElementById("input_num").value
   console.log(input_number)
-  let score = 100 - i;
+  let score = 10 - i;
   i++
-  if (i <= 100) {
+  if (i <= 10) {
     if (input_number < 1 || input_number > 100) {
       document.getElementById("invalid-number").style.display = "initial"
       document.getElementById("result").innerHTML = ""
@@ -18,7 +18,7 @@ const f1 = () => {
     else {
       document.getElementById("invalid-number").style.display = "none"
       if (input_number == guessed_number) {
-        document.getElementById("result").innerHTML = `That's Correct!! Your Score is ${score}/100`
+        document.getElementById("result").innerHTML = `That's Correct!! Your Score is ${score}/10`
         document.getElementById("num_sub").style.display = "none"
       }
       else if (input_number > guessed_number) {
@@ -30,7 +30,7 @@ const f1 = () => {
     }
   }
   else {
-    document.getElementById("result").innerHTML = `You Lost The Number Was ${guessed_number}`
+    document.getElementById("result").innerHTML = `You Lost, The Number Was ${guessed_number}`
     document.getElementById("num_sub").style.display = "none"
   }
 }
