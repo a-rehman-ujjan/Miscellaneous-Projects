de215433ae0f4e0ffb3c5fffe9a5e27b8ffa7f32
Abdul Rehman Ujjan <117349681+a-rehman-ujjan@users.noreply.github.com>
2024-04-11 18:54:56 +0500

    Create script.js

---
+++ Random Color Generator/script.js
@@ -0,0 +1,29 @@
+function getRandomHexCode() {
+  const characters = '0123456789ABCDEF';
+  let hexCode = '#';
+  for (let i = 0; i < 6; i++) {
+    hexCode += characters[Math.floor(Math.random() * 16)];
+  }
+  return hexCode;
+}
+
+function generateRandomColor(){
+  for (let i = 1; i < 6; i++){
+    x = getRandomHexCode();
+    document.getElementById(`colorBox${i}`).style.backgroundColor = x;
+    document.getElementById(`cb${i}Button`).className = '';
+    document.getElementById(`cb${i}Button`).classList.add(x);
+    document.getElementById(`cb${i}Code`).className = '';
+    document.getElementById(`cb${i}Code`).innerHTML = `${x}`;
+  }
+}
+
+function copytoclipboard(id){
+  txt = document.getElementById(id).className;
+  navigator.clipboard.writeText(txt);
+  document.getElementById(id).textContent = 'Copied';
+  document.getElementById(id).classList.add("copied")
+  setTimeout(() => {
+    document.getElementById(id).textContent = 'Copy';
+  }, 1000);
+}
